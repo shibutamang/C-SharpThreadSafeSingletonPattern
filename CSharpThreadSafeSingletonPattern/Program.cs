a386088cb47dedb572aab9a402df4b3b339b2e6d
Shibu Tamang <lamashibu19@gmail.com>
2018-09-03 20:44:39 +0545

    singleton pattern design pattern implemented

--- CSharpThreadSafeSingletonPattern/Program.cs
+++ CSharpThreadSafeSingletonPattern/Program.cs
@@ -10,6 +10,25 @@ namespace CSharpThreadSafeSingletonPattern
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("****** Using Thread Safe Singleton design pattern *****");
+
+            PrintEmployeeDetails();
+            PrintStudentDetails();
+
+            Console.ReadKey();
+        }
+
+        private static void PrintEmployeeDetails()
+        {
+            Singleton _singleton = Singleton.GetInstance();
+
+            _singleton.PrintDetails("Employee Name: Brock Allen");
+        }
+
+        private static void PrintStudentDetails()
+        {
+            Singleton _singleton = Singleton.GetInstance();
+            _singleton.PrintDetails("Student Name: Jeremy Foster");
         }
     }
 }
---
+++ CSharpThreadSafeSingletonPattern/Singleton.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpThreadSafeSingletonPattern
+{
+    public sealed class Singleton
+    {
+        private static readonly object obj = new object();
+        
+        //A private constructor
+        private Singleton()
+        {
+        }
+        //creating default null instanciate
+        private static Singleton _instance = null;
+
+        public static Singleton GetInstance()
+        {
+            if (_instance == null)
+            {
+                lock (obj)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Singleton();
+                    }
+                }
+            }
+            return _instance;
+        }
+
+        //public method which can be accessed with the single instance
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
